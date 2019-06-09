f9b27e1d3e4c696d4896a472a84c9e00b9b15dd0
xRA1N <15974550+xRA1N@users.noreply.github.com>
2019-06-09 10:10:17 +0800

    Create ENH12864_ESP8266_arduino_u8g2.ino

---
+++ ENH12864-130/ENH12864_ESP8266_arduino_u8g2.ino
@@ -0,0 +1,19 @@
+//      ENH12864-130/ ESP8266 arduino    横屏液晶 UC1604液晶
+//     /*sclk=*/ D5, /*data=*/ D7, /*cd=*/ D1, /*cs=*/ D2, /*reset=*/ D9);
+
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Arduino.h>
+U8G2_UC1604_JLX19264_1_4W_HW_SPI u8g2(U8G2_R2,/* cs=*/ D2, /* dc=*/ D1, /* reset=*/ D9); 
+void setup(void) {
+  u8g2.begin();
+}
+
+void loop(void) {
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0, 18, "Hello");    u8g2.drawStr(20, 48, "World!");
+  } while ( u8g2.nextPage() );
+}
